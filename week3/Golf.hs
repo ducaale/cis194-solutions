e36a15f8e5763c82ce3054b62d26ba94518e85be
ducaale <sharaf.13@hotmail.com>
2020-11-10 15:29:20 +0000

    solve homework3

---
+++ week3/Golf.hs
@@ -0,0 +1,29 @@
+module Golf where
+
+import Data.List  (group, sort)
+import Data.Maybe (fromMaybe)
+
+skips :: [a] -> [[a]]
+skips string = map (\n -> skip n string) [1 .. length string]
+  where
+    -- takses a word and filters chars whose indices is divisable by n
+    skip n = map snd . filter ((\x -> x `mod` n == 0) . fst) . zip [1..]
+
+localMaxima :: [Integer] -> [Integer]
+localMaxima (x1:x2:x3:xs) =
+  if x1 < x2 && x2 > x3 then x2 : remaining else remaining
+  where
+    remaining = localMaxima (x2:x3:xs)
+localMaxima _ = []
+
+histogram :: [Integer] -> String
+histogram data' = histogram' (freqFromZeroToNine data') ++ "\n" ++ footer
+  where
+    freq = map (\x -> (head x, length x)) . group . sort
+    freqFromZeroToNine nums = map (\x -> fromMaybe 0 . lookup x . freq $ nums) [0..9]
+    footer = (take 10 $ repeat '=') ++ "\n" ++ ['0'..'9']
+    histogram' nums
+      | all (< 1) nums = []
+      | otherwise =
+         (histogram' $ map pred nums) ++ "\n" ++ (map (\n -> if n > 0 then '*' else ' ') nums)
+
